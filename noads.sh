2d15dc89eb78b68a038efa89022825105d4853b0
a4a031kf7p <102428269+a4a031kf7p@users.noreply.github.com>
2025-01-26 07:32:19 -0300

    comments

--- noads.sh
+++ noads.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# Getting youtube url
 yt_url=$1
+
+# Grab the id of the Youtube video and replace the new id in the iframe.
 yt_id=$(echo "$yt_url" | sed -n 's/.*?v=\([A-Za-z0-9_-]\{11\}\).*/\1/p')
 echo "var newVideoId = \"$yt_id\"; var iframe = document.getElementById('_ytplayer1610'); if (iframe) { iframe.src = \"https://www.youtube.com/embed/\" + newVideoId + \"?autoplay=1&enablejsapi=1&origin=https://www.bing.com&rel=0&mute=0\"; console.log(\"Iframe video has been replaced.\"); } else { console.log(\"Iframe not found.\"); }" | xclip -selection clipboard
